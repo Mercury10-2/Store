904a6360bf68f520144d3784beee98d1037e91a0
Mercury10-2 <55573544+Mercury10-2@users.noreply.github.com>
2020-06-09 18:47:13 +0300

    Update StoreController.java

--- src/main/java/store/controller/StoreController.java
+++ src/main/java/store/controller/StoreController.java
@@ -149,4 +149,3 @@ public class StoreController {
         return "redirect:/";
     }
 }
-//  Create, update, delete, search, filter and sort functionality with error checking, pop-up messages and highlights.
